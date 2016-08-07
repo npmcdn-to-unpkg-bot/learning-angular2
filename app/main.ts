33b853fcb39d3916892ec84fb949d7d52313fb1b
Josh Tyler <josh_tyler1994@hotmail.com>
2016-08-07 18:03:41 +0100

    Routing Stage

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { bootstrap } from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 
 bootstrap(AppComponent);
- 
+
